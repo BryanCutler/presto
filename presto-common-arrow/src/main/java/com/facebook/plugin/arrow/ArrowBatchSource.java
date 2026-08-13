08f77af0b8953e51d62f7724a1457d3a815edbd2
Bryan Cutler <cutlerb@gmail.com>
2026-08-13 10:40:14 -0700

    Revert ArrowBatchSource changes

--- presto-common-arrow/src/main/java/com/facebook/plugin/arrow/ArrowBatchSource.java
+++ presto-common-arrow/src/main/java/com/facebook/plugin/arrow/ArrowBatchSource.java
@@ -17,7 +17,6 @@ import com.facebook.presto.common.Page;
 import com.facebook.presto.common.block.Block;
 import com.facebook.presto.spi.ColumnMetadata;
 import com.facebook.presto.spi.ConnectorPageSource;
-import com.facebook.presto.spi.connector.ConnectorArrowSourceBase;
 import com.google.common.collect.ImmutableList;
 import org.apache.arrow.memory.BufferAllocator;
 import org.apache.arrow.vector.AllocationHelper;
@@ -30,17 +29,82 @@ import java.util.List;
 import static com.facebook.plugin.arrow.BlockArrowWriter.createArrowWriters;
 import static java.util.Objects.requireNonNull;
 
-public abstract class ArrowBatchSource
+public class ArrowBatchSource
         implements AutoCloseable
 {
-    public static ArrowBatchSource create(BufferAllocator allocator, List<ColumnMetadata> columns, ConnectorPageSource pageSource, int maxRowsPerBatch)
+    private final VectorSchemaRoot root;
+    private final List<BlockArrowWriter.ArrowVectorWriter> writers;
+    private final int maxRowsPerBatch;
+    private final ConnectorPageSource pageSource;
+
+    private Page currentPage;
+    private int currentPosition;
+
+    public ArrowBatchSource(BufferAllocator allocator, List<ColumnMetadata> columns, ConnectorPageSource pageSource, int maxRowsPerBatch)
     {
-        return new ArrowBatchFromBlock(allocator, columns, pageSource, maxRowsPerBatch);
+        this.pageSource = requireNonNull(pageSource, "pageSource is null");
+        this.maxRowsPerBatch = maxRowsPerBatch;
+        ImmutableList.Builder<BlockArrowWriter.ArrowVectorWriter> writerBuilder = ImmutableList.builder();
+        this.root = createArrowWriters(allocator, columns, writerBuilder);
+        this.writers = writerBuilder.build();
     }
 
-    public static ArrowBatchSource create(BufferAllocator allocator, List<ColumnMetadata> columns, ConnectorArrowSourceBase pageSource, int maxRowsPerBatch)
+    public VectorSchemaRoot getVectorSchemaRoot()
     {
-        return null;
+        return root;
+    }
+
+    /**
+     * Loads the next record batch from the source.
+     * Returns false if there are no more batches from the source.
+     */
+    public boolean nextBatch()
+    {
+        // Release previous buffers
+        root.clear();
+
+        // Reserve capacity for next batch
+        allocateVectorCapacity(root, maxRowsPerBatch);
+
+        int batchRowIndex = 0;
+        while (batchRowIndex < maxRowsPerBatch) {
+            if (currentPage == null || currentPosition >= currentPage.getPositionCount()) {
+                if (pageSource.isFinished()) {
+                    break;
+                }
+
+                currentPage = pageSource.getNextPage();
+                currentPosition = 0;
+
+                if (currentPage == null || currentPage.getPositionCount() == 0) {
+                    continue;
+                }
+            }
+
+            for (int column = 0; column < writers.size(); column++) {
+                Block block = currentPage.getBlock(column);
+                BlockArrowWriter.ArrowVectorWriter writer = writers.get(column);
+
+                if (block.isNull(currentPosition)) {
+                    writer.writeNull(batchRowIndex);
+                }
+                else {
+                    writer.writeBlock(batchRowIndex, block, currentPosition);
+                }
+            }
+            currentPosition++;
+            batchRowIndex++;
+        }
+        root.setRowCount(batchRowIndex);
+        return batchRowIndex > 0;
+    }
+
+    @Override
+    public void close()
+            throws IOException
+    {
+        root.close();
+        pageSource.close();
     }
 
     private static void allocateVectorCapacity(VectorSchemaRoot root, int capacity)
@@ -48,94 +112,6 @@ public abstract class ArrowBatchSource
         for (ValueVector vector : root.getFieldVectors()) {
             vector.setInitialCapacity(capacity);
             AllocationHelper.allocateNew(vector, capacity);
-        }
-    }
-
-    /**
-     * Returns the VectorSchemaRoot for the Arrow batches.
-     */
-    public abstract VectorSchemaRoot getVectorSchemaRoot();
-
-    /**
-     * Loads the next record batch from the source.
-     * Returns false if there are no more batches from the source.
-     */
-    public abstract boolean nextBatch();
-
-    private static class ArrowBatchFromBlock
-            extends ArrowBatchSource
-    {
-        private final VectorSchemaRoot root;
-        private final List<BlockArrowWriter.ArrowVectorWriter> writers;
-        private final int maxRowsPerBatch;
-        private final ConnectorPageSource pageSource;
-
-        private Page currentPage;
-        private int currentPosition;
-
-        public ArrowBatchFromBlock(BufferAllocator allocator, List<ColumnMetadata> columns, ConnectorPageSource pageSource, int maxRowsPerBatch)
-        {
-            this.pageSource = requireNonNull(pageSource, "pageSource is null");
-            this.maxRowsPerBatch = maxRowsPerBatch;
-            ImmutableList.Builder<BlockArrowWriter.ArrowVectorWriter> writerBuilder = ImmutableList.builder();
-            this.root = createArrowWriters(allocator, columns, writerBuilder);
-            this.writers = writerBuilder.build();
-        }
-
-        @Override
-        public VectorSchemaRoot getVectorSchemaRoot()
-        {
-            return root;
-        }
-
-        @Override
-        public boolean nextBatch()
-        {
-            // Release previous buffers
-            root.clear();
-
-            // Reserve capacity for next batch
-            allocateVectorCapacity(root, maxRowsPerBatch);
-
-            int batchRowIndex = 0;
-            while (batchRowIndex < maxRowsPerBatch) {
-                if (currentPage == null || currentPosition >= currentPage.getPositionCount()) {
-                    if (pageSource.isFinished()) {
-                        break;
-                    }
-
-                    currentPage = pageSource.getNextPage();
-                    currentPosition = 0;
-
-                    if (currentPage == null || currentPage.getPositionCount() == 0) {
-                        continue;
-                    }
-                }
-
-                for (int column = 0; column < writers.size(); column++) {
-                    Block block = currentPage.getBlock(column);
-                    BlockArrowWriter.ArrowVectorWriter writer = writers.get(column);
-
-                    if (block.isNull(currentPosition)) {
-                        writer.writeNull(batchRowIndex);
-                    }
-                    else {
-                        writer.writeBlock(batchRowIndex, block, currentPosition);
-                    }
-                }
-                currentPosition++;
-                batchRowIndex++;
-            }
-            root.setRowCount(batchRowIndex);
-            return batchRowIndex > 0;
-        }
-
-        @Override
-        public void close()
-                throws IOException
-        {
-            root.close();
-            pageSource.close();
         }
     }
 }
